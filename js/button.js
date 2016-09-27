fdd48c80e4f70e477f72f16e0b219d8c791c4986
Jacqueline Linang <jacquelinang@Jacquelines-MacBook-Pro-4.local>
2016-09-27 10:45:41 -0400

    v2.4
    After button disappears, page is redirected.

--- js/button.js
+++ js/button.js
@@ -1,24 +1,28 @@
-var myVar = setInterval(myTimer, 1000);
+var myVar = setInterval(fade, 1000);
 var opacityLevel = 1;
 
-function myTimer() {
+function fade() {
      var button = document.getElementsByClassName("cd-optionals");
     
     if (opacityLevel > 0)
         {
             opacityLevel -= .1;
             var newOpacity = opacityLevel.toString();
+            button[0].style.opacity = newOpacity;
+            button[1].style.opacity = newOpacity;
+            
+            /*
+            if((opacityLevel / .2) == 0){
+                button[0].style.borderStyle = "solid";
+                button[1].style.borderStyle = "solid";
+                button[0].style.borderWidth = "5px";
+                button[1].style.borderWidth = "5px";
+                button[0].style.borderColor = "red";
+                button[1].style.borderColor = "red";
+            }
+            */
         }
+    else(window.location.pathname = "scene2.html");
     
-    if((opacityLevel / 2) == 0){
-        button[0].style.borderStyle = "solid";
-        button[1].style.borderStyle = "solid";
-        button[0].style.borderWidth = "5px";
-        button[1].style.borderWidth = "5px";
-        button[0].style.borderColor = "red";
-        button[1].style.borderColor = "red";
-    }
     
-    button[0].style.opacity = newOpacity;
-    button[1].style.opacity = newOpacity;
 }
